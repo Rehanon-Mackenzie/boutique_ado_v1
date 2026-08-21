844baebf0b3299dad9ffb3c932ba023e956c3711
Rehanon-Mackenzie <fairywings38@hotmail.com>
2026-08-21 19:39:53 +0100

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -13,7 +13,9 @@ def checkout(request):
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51U6vPdHF9Vbq2m2QBbIddo6QHe9buyocJWR3bOR7fnF71VOpqv3rWt8m3F15XzfPCqT4g0Fw1oSs3C4XfMOgFP4r00iky5IHz3',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
